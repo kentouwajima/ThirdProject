fa2e5902fd8eff567db2136c7f1cf1abc365d6d8
kentouwajima <u.kento6366@gmail.com>
2024-10-07 09:27:50 +0900

    firstcommit

---
+++ src/test/java/com/example/ThirdProject/ThirdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThirdProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
