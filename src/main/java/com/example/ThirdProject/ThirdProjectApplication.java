4153c46c122d5d3465fb33ceb7eea73cf12ebd90
kentouwajima <u.kento6366@gmail.com>
2024-10-07 13:11:02 +0900

    GET、POSTメソッドの実装

--- src/main/java/com/example/ThirdProject/ThirdProjectApplication.java
+++ src/main/java/com/example/ThirdProject/ThirdProjectApplication.java
@@ -3,19 +3,34 @@ package com.example.ThirdProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ThirdProjectApplication {
 
+	private String name = "Enami Kouji";
+	private String age = "37";
+
 	public static void main(String[] args) {
 		SpringApplication.run(ThirdProjectApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "hello world";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo(){
+		return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age){
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name){
+		this.name = name;
 	}
 
 }
